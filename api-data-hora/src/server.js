7a5fe595ead8e428e0f45264c26da379b33ea5b7
Rafael Mizobuchi <rafaelmizob@gmail.com>
2026-05-01 14:52:06 -0300

    .

---
+++ api-data-hora/src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('API funcionando 🚀');
+});
+
+app.get('/data-hora', (req, res) => {
+  const agora = new Date();
+
+  res.json({
+    data: agora.toLocaleDateString('pt-BR'),
+    hora: agora.toLocaleTimeString('pt-BR')
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
